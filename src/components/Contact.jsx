3d048e098c0797e9a9c936128ee640d5e50176a8
Alan Guo <alan.t.guo@gmail.com>
2024-11-15 04:16:18 +0000

    removed address correct

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,21 +11,21 @@ const Contact = () => {
         viewport={{ once: true }}
         className="my-10 text-center text-4xl">Get in Touch</motion.h2>
         <div className="text-center tracking-tighter">
-            <motion.p 
+            {/* <motion.p 
             whileInView={{opacity:1,x:0}}
             initial={{opacity:0,x:-100}}
             transition={{duration:1}}
             viewport={{ once: true }}
-            className="my-4">{CONTACT.address}</motion.p>
+            className="my-4">{CONTACT.address}</motion.p> */}
             <motion.p 
             whileInView={{opacity:1,x:0}}
             initial={{opacity:0,x:100}}
             transition={{duration:1}}
             viewport={{ once: true }}
             className="my-4">{CONTACT.phoneNo}</motion.p>
-            {/* <a href="#" className="border-b">
+            <a href="#" className="border-b">
                 {CONTACT.email}
-            </a> */}
+            </a>
         </div>
     </div>
   )
