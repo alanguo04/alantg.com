de986f08f8b6249b00d9cb838d024756decdf132
Alan Guo <alan.t.guo@gmail.com>
2024-12-12 01:41:14 +0000

    redo2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        forkhloe: resolve(__dirname, 'forkhloe/index.html'),
+      },
+    },
+  },
   plugins: [react()],
 })
